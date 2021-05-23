11f1d0549997e25dd6a29feaf835ed2e8f911ccb
theonlysepp <52838302+theonlysepp@users.noreply.github.com>
2021-05-23 23:53:44 +0200

    debug debugged
    Jetzt mit Zelenumbruechen, und die entsprechende Zeile wird korrekt erkannt. Wirklich.

--- python_scripts/pw_generator.py
+++ python_scripts/pw_generator.py
@@ -2,26 +2,14 @@
 import subprocess
 
 
-def find_all(a_str, sub):
-    start = 0
-    while True:
-        start = a_str.find(sub, start)
-        if start == -1: return
-        yield start
-        start += len(sub) # use start += 1 to find overlapping matches
-
-
 def add_wlan(wlanname, password):
     p = subprocess.Popen(["wpa_passphrase", wlanname, password], stdout=subprocess.PIPE)
     return_stuff = p.communicate()
   
     ascii_str = return_stuff[0].decode('ascii')
 
-    line_list = ascii_str.splitlines()
-    # Zeile mit dem Passwort in Klartext rausschmeissen
-    for i in line_list:
-        if i[0:4] == '#psk':
-            del(line_list[i])
+    # in Zeilen umwandeln, und Zeile mit dem Passwort in Klartext rausschmeissen
+    line_list = [i+'\n' for i in ascii_str.splitlines() if i[0:5] != '\t#psk']
 
     # in die Datei schreiben
     with open('/etc/wpa_supplicant/wpa_supplicant.conf','a') as fobj:
