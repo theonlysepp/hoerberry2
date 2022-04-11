242b8f47b21a1cd53ae502a1b9ac85e987117e13
theonlysepp <52838302+theonlysepp@users.noreply.github.com>
2022-04-11 22:05:16 +0200

    Testskript zum Einstellen des Verstärkers

---
+++ python_scripts/test_scripts/test_volume.py
@@ -0,0 +1,30 @@
+# Test der Verstärkerlautstaerke
+# zufaelliges Abspielen der deutschen Ansagetexte, mit varabler Lautstaerke
+# zum Einstelen des Verstaerkers. Die Hoerbuecher und Lieder sind in der Regel etwas lauter.
+
+from pathlib import Path
+import sys
+import hjson
+from random import shuffle, randint
+import subprocess
+
+# alle relevanten Pfade erzeugen
+p_test_skripts=Path('.').absolute()
+p_python = p_test_skripts.parent
+p_github = p_python.parent
+p_settings = Path(p_github, 'settings_and_data')
+
+# Pfad fuer den Modulimport von doglcd hinzufuegen
+sys.path.append(str(p_python))
+
+# einmalig der allgemeine Ort von basesettings.ini,
+path = Path('/home/pi/audio_messages/deutsch')
+
+soundlist = [e for e in path.iterdir() if e.is_file()]
+
+
+
+while True: 
+    Vol = input('bitte Lautstaerke eingeben: ')
+    subprocess.Popen([f"sudo", "amixer", "set", "Master", f'{Vol}%'], stdout=subprocess.PIPE)
+    subprocess.call(f'sudo mpg123 {soundlist[randint(0,len(soundlist)-1)].resolve()}', shell=True)
